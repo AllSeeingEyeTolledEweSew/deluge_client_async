530c919da5d9a621f3a8643f39642befb503ddfd
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2016-11-12 07:28:39 +0000

    Adding initial files.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+
+from setuptools import setup
+
+with open("README") as readme:
+    documentation = readme.read()
+
+setup(
+    name = "deluge_client_async",
+    version = "0.1.0",
+
+    description = "An asyncio client to deluge.",
+    long_description = documentation,
+    author = "AllSeeingEyeTolledEweSew",
+    author_email = "allseeingeyetolledewesew@protonmail.com",
+    license = "ISC",
+    py_modules=["deluge_client_async"],
+    url = "http://github.com/allseeingeyetolledewesew/deluge_client_async",
+)
