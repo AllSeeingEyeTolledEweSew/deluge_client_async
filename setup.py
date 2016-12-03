36d0e69a777e3a4cce9ad501d2c44387bb6de237
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2016-12-03 15:28:24 -0500

    Add install_requires.

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setup(
     license="Unlicense",
     py_modules=["deluge_client_async"],
     url="http://github.com/allseeingeyetolledewesew/deluge_client_async",
-    python_requires=">=3.4"
+    python_requires=">=3.4",
+    install_requires=[
+        "rencode>=1.0.0"
+    ]
 )
